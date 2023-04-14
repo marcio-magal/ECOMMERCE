3c6a4db82d7ceab026040fb8bcd01eafae9886bc
marcio-magal <marcio.magalhaes.432@gmail.com>
2023-04-14 17:20:08 -0300

    CRUD Básico

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
-
+from django.urls import path, include
+from .views import teste_crud, salvar, editar, update, delete
 from . import views
 
 urlpatterns = [
     path('', views.index),
+
+    path('teste-crud', teste_crud),
+    path('salvar/', salvar, name="salvar"),
+    path('editar/<int:id>', editar, name='editar'),
+    path('update/<int:id>', update, name='update'),
+    path('delete/<int:id>', delete, name='delete'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,30 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Pessoa
 def index(request):
     return render(request, 'index.html')
+
+def teste_crud(request):
+    pessoas = Pessoa.objects.all()
+    return render(request, "Teste CRUD.html", {"pessoas": pessoas})
+
+def salvar(request):
+    vnome = request.POST.get("nome")
+    Pessoa.objects.create(nome=vnome)
+    pessoas = Pessoa.objects.all()
+    return render(request, "Teste CRUD.html", {"pessoas": pessoas})
+
+def editar(request, id):
+    pessoa = Pessoa.objects.get(id=id)
+    return render(request, "update.html", {"pessoa": pessoa})
+
+def update(request, id):
+    vnome = request.POST.get("nome")
+    pessoa = Pessoa.objects.get(id=id)
+    pessoa.nome = vnome
+    pessoa.save()
+    return redirect(teste_crud)
+
+def delete(request, id):
+    pessoa = Pessoa.objects.get(id=id)
+    pessoa.delete()
+    return redirect(teste_crud)
